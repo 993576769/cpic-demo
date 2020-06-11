e5b2cf8666899ec933a27597926dafb88938bc4a
dave <dave@beansmile.com>
2020-06-11 15:54:37 +0800

    feat: add update check

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <script>
   /* eslint-disable pathname/match-name */
+  import { confirm } from '@/utils';
+
   export default {
-		onLaunch: function() {
-		},
-		onShow: function() {
-		},
-		onHide: function() {
-		}
-	}
+    onShow() {
+      this.checkUpdateManager();
+    },
+    methods: {
+      // 新版本提示
+      checkUpdateManager() {
+        const updateManager = uni.getUpdateManager();
+        updateManager.onUpdateReady(async() => {
+          await confirm('新版本已经准备好，是否重启应用？')
+          updateManager.applyUpdate();
+        })
+      }
+    }
+  }
 </script>
 
 <style>
-	/*每个页面公共css */
+  /*每个页面公共css */
+  Page {
+    font-size: 14px;
+    padding-bottom: env(safe-area-inset-bottom);
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
 </style>
