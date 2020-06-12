600cb1f91d069038660b533e95f00e177c8bdc76
jamie <jamie@beansmile.com>
2020-06-12 10:39:38 +0800

    feat: 交互反馈整合

---
+++ src/utils/prompt.js
@@ -0,0 +1,112 @@
+import Vue from 'vue'
+
+export function showToast(params) {
+  const options = {
+    title: '',
+    icon: 'none',
+    mask: true,
+    duration: 1500,
+  }
+  if (typeof params === 'string') {
+    Object.assign(options, { title: params })
+  } else {
+    Object.assign(options, params)
+  }
+  uni.showToast(options)
+  return new Promise(resolve => setTimeout(resolve, options.duration))
+}
+
+export function showLoading(params) {
+  const options = {
+    title: '',
+    mask: true,
+  }
+  if (typeof params === 'string') {
+    Object.assign(options, { title: params })
+  } else {
+    Object.assign(options, params)
+  }
+  return uni.showLoading(options)
+}
+
+export const alert = (content, opts = {}) => {
+  return uni.showModal({
+    content,
+    confirmText: '我知道了',
+    showCancel: false,
+    ...opts
+  });
+}
+
+export const confirm = async (content, opts = {}) => {
+  const { confirm } = await uni.showModal({
+    content,
+    confirmText: '确认',
+    showCancel: true,
+    cancelText: '取消',
+    ...opts
+  });
+  if (!confirm) {
+    return Promise.reject(new Error('用户取消'));
+  }
+}
+
+export function autoLoadingDecorator(target, name, descriptor) {
+  const func = descriptor.value
+  descriptor.value = function () {
+    return autoLoading(func.apply(this, arguments))
+  }
+}
+
+export function autoLoading(target, options) {
+  showLoading(options || '加载中')
+  const action = Promise.resolve(target instanceof Function ? target() : target)
+  return action
+    .finally(() => {
+      uni.hideLoading()
+    })
+    .catch(err => {
+      errHandle(err)
+    })
+}
+
+export function pageRefresh(target, name, descriptor) {
+  const func = descriptor.value
+  descriptor.value = async function () {
+    try {
+      await func.apply(this, arguments)
+    } catch (err) {
+      errHandle(err)
+    } finally {
+      uni.stopPullDownRefresh()
+    }
+  }
+}
+
+export function errToast(target, name, descriptor) {
+  const func = descriptor.value
+  descriptor.value = function () {
+    return func.apply(this, arguments).catch(err => {
+      uni.hideLoading()
+      errHandle(err)
+    })
+  }
+}
+
+export function errHandle(err) {
+  const ignoreErrors = /(cancel|ignore|请先登录)/i
+  const msg = err.message || err.errMsg
+  if (!ignoreErrors.test(msg)) {
+    msg && alert(msg, {
+      title: '请求失败',
+    })
+  }
+  throw err
+}
+
+Vue.prototype.$showToast = showToast
+Vue.prototype.$showLoading = showLoading
+Vue.prototype.$autoLoading = autoLoading
+Vue.autoLoading = autoLoadingDecorator
+Vue.pageRefresh = pageRefresh
+Vue.errToast = errToast
