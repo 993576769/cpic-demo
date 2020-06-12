e31aec9290cecbec71637e62be5ee6005601d343
jamie <jamie@beansmile.com>
2020-06-12 10:12:56 +0800

    feat: 添加 eslint

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script>
-	export default {
+  /* eslint-disable pathname/match-name */
+  export default {
 		onLaunch: function() {
-			console.log('App Launch')
 		},
 		onShow: function() {
-			console.log('App Show')
 		},
 		onHide: function() {
-			console.log('App Hide')
 		}
 	}
 </script>
